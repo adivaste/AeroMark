1c2d0a4cdff436e002e7576ebd0893ae48ce8774
adivaste <adivaste@outlook.com>
2023-08-15 01:00:18 +0530

    Fixed backdrop for Bookmarks

--- AeroMark/urls.py
+++ AeroMark/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from webapp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', index, name='index')
+    path('', views.index, name='index')
 ]
-
